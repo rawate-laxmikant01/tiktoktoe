f83ba13af9d97a270b7082e3ed9803e7a0971384
Laxmikant <rawatelaxmikantslr@gmail.com>
2021-06-06 11:00:50 +0530

    tik-tac-toe

--- app/src/main/java/com/example/tiktactoe/MainActivity.java
+++ app/src/main/java/com/example/tiktactoe/MainActivity.java
@@ -16,96 +16,91 @@ import static java.lang.System.exit;
 public class MainActivity extends AppCompatActivity {
 
     boolean gameActive = true;
-    //0=x;
-    //1=o;
-    //2=null;
+    // Player representation
+    // 0 - X
+    // 1 - O
     int activePlayer = 0;
-    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
-    int[][] winPositions = {{0, 1, 2,}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+    int[] gameState = {2, 2 , 2, 2, 2, 2, 2, 2, 2,2};
+    //    State meanings:
+    //    0 - X
+    //    1 - O
+    //    2 - Null
+    int[][] winPositions = {{0,1,2}, {3,4,5}, {6,7,8},
+            {0,3,6}, {1,4,7}, {2,5,8},
+            {0,4,8}, {2,4,6}};
+    public void playerTab(View view){
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+        if(!gameActive){
+            gameReset(view);
+        }
+        if(gameState[tappedImage] == 2) {
+            gameState[tappedImage] = activePlayer;
+            img.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                img.setImageResource(R.drawable.x);
+                activePlayer = 1;
+                TextView status = findViewById(R.id.status);
+                status.setText("O's Turn - Tap to play");
+            } else {
+                img.setImageResource(R.drawable.o);
+                activePlayer = 0;
+                TextView status = findViewById(R.id.status);
+                status.setText("X's Turn - Tap to play");
+            }
+            img.animate().translationYBy(1000f).setDuration(300);
+        }
+        // Check if any player has won
+        for(int[] winPosition: winPositions){
+            if(gameState[winPosition[0]] == gameState[winPosition[1]] &&
+                    gameState[winPosition[1]] == gameState[winPosition[2]] &&
+                    gameState[winPosition[0]]==gameState[winPosition[2]]&&
+                    gameState[winPosition[0]]!=2){
+                // Somebody has won! - Find out who!
+                String winnerStr;
+                gameActive = false;
+                if(gameState[winPosition[0]] == 0){
+                    winnerStr = "X has won";
+                }
+                else{
+                    winnerStr = "O has won";
+                }
+                // Update the status bar for winner announcement
+                TextView status = findViewById(R.id.status);
+                status.setText(winnerStr);
 
+            }
+
+
+
+        }
+
+    }
+
+    public void gameReset(View view) {
+        gameActive = true;
+        activePlayer = 0;
+        for(int i=0; i<gameState.length; i++){
+            gameState[i] = 2;
+        }
+        ((ImageView)findViewById(R.id.image_view_0)).setImageResource(0);
+        ((ImageView)findViewById(R.id.image_view_1)).setImageResource(0);
+        ((ImageView)findViewById(R.id.image_view_2)).setImageResource(0);
+        ((ImageView)findViewById(R.id.image_view_3)).setImageResource(0);
+        ((ImageView)findViewById(R.id.image_view_4)).setImageResource(0);
+        ((ImageView)findViewById(R.id.image_view_5)).setImageResource(0);
+        ((ImageView)findViewById(R.id.image_view_6)).setImageResource(0);
+        ((ImageView)findViewById(R.id.image_view_7)).setImageResource(0);
+        ((ImageView)findViewById(R.id.image_view_8)).setImageResource(0);
+
+        TextView status = findViewById(R.id.status);
+        status.setText("X's Turn - Tap to play");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-    }
-
-    public void playerTab(View view) {
-        ImageView img = (ImageView) view;
-        //created imgage view and taking tag of image
-
-        int tappedImage = Integer.parseInt(img.getTag().toString());
-
-        if (gameState[tappedImage] == 2) {
-            gameState[tappedImage] = activePlayer;
-            img.setTranslationY(-1000f);
-
-            if (activePlayer == 0) {
-                img.setImageResource(R.drawable.x);
-                activePlayer = 1;
-
-                TextView status = findViewById(R.id.status);
-                status.setText("O 's Turn -Tap to play");
-            } else {
-                img.setImageResource(R.drawable.o);
-                activePlayer = 0;
-                TextView status = findViewById(R.id.status);
-                status.setText("X 's Turn -Tap to play");
-            }
-
-            img.animate().translationYBy(1000f).setDuration(300);
-        }
-
-        // check if any player has won
-
-//        for(int[] winpositon :winPositions){
-//
-//            if(gameState[winpositon[0]]==gameState[winpositon[1]] &&
-//                    gameState[winpositon[1]]==gameState[winpositon[2]] &&
-//                        gameState[winpositon[0]]!=2){
-
-        for (int[] winPosition : winPositions) {
-            if (gameState[winPosition[0]] == gameState[winPosition[1]] &&
-                    gameState[winPosition[1]] == gameState[winPosition[2]] &&
-                    gameState[winPosition[0]] != 2) {
-
-                //Someone is won lets find out Who
-                String winnerStr;
-                gameActive = false;
-
-                if (gameState[winPosition[0]] == 0) {
-                    winnerStr = "X has Won";
-
-                } else {
-                    winnerStr = "O has Won";
-                }
-
-                TextView status = findViewById(R.id.status);
-                status.setText(winnerStr);
-
-
-            }
-
-        }
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.restart_id:
-                Toast.makeText(this, "Restart game", Toast.LENGTH_SHORT).show();
-                break;
-
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
